8ebf19446a17dbf05f571d84982d84bf1a2744d3
Kenneth <rimorinkennet@gmail.com>
2020-02-28 15:54:58 +1100

    Generated Listing Controller

--- Zhus_Marketplace/app/models/listing.rb
+++ Zhus_Marketplace/app/models/listing.rb
@@ -1,2 +1,4 @@
 class Listing < ApplicationRecord
+    belongs_to :brand
+    enum category: {casual:0, formal:1, basketball:2, running:3, skate&canvas:4, other: 5}
 end
--- Zhus_Marketplace/config/routes.rb
+++ Zhus_Marketplace/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'listings/index'
+  get 'listings/create'
+  get 'listings/new'
+  get 'listings/show'
+  get 'listings/edit'
+  get 'listings/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
